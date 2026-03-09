fe2aa7a0ff8c822eb796923a3ed86503a9967eac
DeviousWind543 <98923520+DeviousWind543@users.noreply.github.com>
2026-03-09 17:33:10 -0500

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/IDD_FARO',
+  // Solo usar basePath en producción
+  basePath: process.env.NODE_ENV === 'production' ? '/IDD_FARO' : '',
   images: {
     unoptimized: true,
   },
@@ -10,7 +11,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   devIndicators: false,
-  // ⚠️ Importante para ignorar errores durante el build
   eslint: {
     ignoreDuringBuilds: true,
   },
