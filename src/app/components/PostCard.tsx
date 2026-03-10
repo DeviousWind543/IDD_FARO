cf8b4541d0bd22bd1620f2207a2099254929ce07
DeviousWind543 <98923520+DeviousWind543@users.noreply.github.com>
2026-03-09 21:03:31 -0500

    deploy

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -40,6 +40,7 @@ export interface Post { // Exportado para ser usado en TeacherPanel.tsx
 
 interface PostCardProps {
   post: Post;
+  theme: 'dark' | 'light';
   currentUserId: string | number | null; // Asegúrate de que el tipo sea consistente
   onDeletePost: (post: Post) => void; // CAMBIO AQUÍ: Ahora espera el objeto Post completo
   onStartEditPost: (post: Post) => void;
